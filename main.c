dc2fb092c843df5a994a19a3bc3bd6b59c1c75e4
OmarCodes022 <omar.ahmed@epitech.eu>
2024-11-18 11:31:38 +0100

    test le branch

--- main.c
+++ main.c
@@ -1,14 +1,5 @@
 #include <stdio.h>
 
-void transform_numbers(int *a, int *b)
-{
-    if (a == NULL || b == NULL) {
-        return;
-    }
-    *a += *b;         
-    *b = *a - *b;     
-    *a -= *b;                  
-}
 
 void display_result(int result)
 {
