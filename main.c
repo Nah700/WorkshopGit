eac4dbf0e495e9b650093f7e99e417d4214cafa8
Sarouti Younes <younes.sarouti@epitech.eu>
2024-11-18 10:59:33 +0100

    commit transform_numbers

--- main.c
+++ main.c
@@ -1,17 +1,5 @@
 #include <stdio.h>
 
-void display_result(int result)
-{
-    printf("The result of the sum is %d.\n", result);
-}
-
-int main(void)
-{
-    int a = 5;
-    int b = 15;
-}
-
-
 void transform_numbers(int *a, int *b)
 {
     if (a == NULL || b == NULL) {
@@ -21,3 +9,9 @@ void transform_numbers(int *a, int *b)
     *b = *a - *b;     
     *a -= *b;                  
 }
+
+void display_result(int result)
+{
+    printf("The result of the sum is %d.\n", result);
+}
+
