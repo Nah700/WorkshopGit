e342238b37f3765bce6befa997a8939385b59ef1
olivier <olivier.zingraff.schiltz@gmail.com>
2024-11-18 10:48:31 +0100

    add:fonction pour transformer les nb

--- main.c
+++ main.c
@@ -5,6 +5,12 @@ void display_result(int result)
     printf("The result of the sum is %d.\n", result);
 }
 
+void transform_numbers(int *a, int *b)
+{
+    *a = *a * 2;
+    *b = *b + 3;
+}
+
 int main(void)
 {
     int a = 5;
@@ -12,7 +18,7 @@ int main(void)
 
     // Ajouter une fonction qui effectue plusieurs opérations sur les deux variables prototyper de la sorte:
     // void transform_numbers(int *a, int *b);
-    // L'appeler ici à la place de ce commentaire
+    transform_numbers(&a, &b);
     display_result(a + b);
     return 0;
 }
