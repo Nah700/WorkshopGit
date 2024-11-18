f445889fe605995f558fdec9ff2bb5af8e45c249
Nahim KANDARI <nahimkandari@pop-os.localdomain>
2024-11-18 10:44:07 +0100

    [FIX]variable name | print strign

--- main.c
+++ main.c
@@ -2,17 +2,18 @@
 
 void display_result(int result)
 {
-    printf("The result of the sum is %d.\n", result);
+    printf("The result of the sum is the number %d.\n", result);
 }
 
 int main(void)
 {
-    int a = 5;
+    int ah_jadore_ce_nombre = 5000;
     int b = 15;
 
     // Ajouter une fonction qui effectue plusieurs opérations sur les deux variables prototyper de la sorte:
     // void transform_numbers(int *a, int *b);
     // L'appeler ici à la place de ce commentaire
-    display_result(a + b);
+    display_result(ah_jadore_ce_nombre + b);
+    dprintf(1, "It work !\n");
     return 0;
 }
