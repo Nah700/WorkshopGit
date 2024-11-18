b6a3c9241a339d8a38be21422afdaed31946af4f
G0LDDARK <g0lddark.linux@gmail.com>
2024-11-18 11:53:07 +0100

    [FIX] : Restore the code that was slain by Noé : (

--- main.c
+++ main.c
@@ -1,1 +1,40 @@
-OUI OUOI UO 
+/*
+** EPITECH PROJECT, 2024
+** WorkshopGit
+** File description:
+** main
+*/
+
+#include <stdio.h>
+
+void transform_numbers(int *a, int *b)
+{
+    if (a == NULL || b == NULL) {
+        return;
+    }
+    *a += *b;
+    *b = *a - *b;
+    *a -= *b;
+}
+
+void display_result(int result)
+{
+    printf("The result of the sum is %d.\n", result);
+}
+
+void transform_number(int *a, int *b)
+{
+    *a += 4;
+    *b = *a * 4;
+}
+
+int main(void)
+{
+    int a = 5;
+    int b = 15;
+
+    transform_number(&a, &b);
+    display_result(a + b);
+    printf("It works !\n");
+    return 0;
+}
