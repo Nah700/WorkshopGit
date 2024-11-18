351547804f6e052ce6d515bb0b9f897309424703
alexandre.romagnoli@epitech.eu <alexandre.romagnoli@epitech.eu>
2024-11-18 10:54:20 +0100

    add fonction additon + reatribution de valeur

--- main.c
+++ main.c
@@ -5,11 +5,18 @@ void display_result(int result)
     printf("The result of the sum is %d.\n", result);
 }
 
+void transform_numbers(int *a, int *b)
+{
+    *a += 4;
+    *b = *a + 4;
+}
+
 int main(void)
 {
     int a = 5;
     int b = 15;
 
+    transform_numbers(&a, &b);
     // Ajouter une fonction qui effectue plusieurs opérations sur les deux variables prototyper de la sorte:
     // void transform_numbers(int *a, int *b);
     // L'appeler ici à la place de ce commentaire
